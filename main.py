66b0b35377fc97a71129fe4cb5821b89293d55e4
Zak <zakismail888@gmail.com>
2024-11-27 07:29:12 +0200

    Day 33: Final Touches

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import plotly.express as px
 from backend import get_data
 
 st.title("Weather Forecast for the Next Days")
+st.write("Only well known cities not what you are thinking")
 place = st.text_input("Place: ")
 days = st.slider("Forecast Days", min_value=1, max_value=5,
                  help="Select the number of forecasted days")
